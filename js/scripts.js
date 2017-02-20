11b211d9ef5df9799a9e348afa0dd96b2e7c4536
gsag <galves@id.uff.br>
2017-02-20 14:19:23 -0300

    Creating script for responsiveness on small screens.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
-$('.js-sidebar-collapse').on('click', function (event) {
-	event.preventDefault();
-	/* Find the i element and toggle the icon class */
-	$(this).find('i').toggleClass('fa-angle-left').toggleClass('fa-angle-right');
-	/* Find the following elements and toggle responsive classes to collapse the sidebar */
-	$('#sidebar-wrapper').toggleClass('collapsed');	
-	$('#sidebar-wrapper nav.nav-sidebar ul.nav > li.nav-item a.nav-link').find('span').toggleClass('hide');
-	$('.logo-text').toggleClass('hide');
-	$('.brand').toggleClass('collapsed');
-	$('#page-wrapper').toggleClass('collapsed');
-	$('header.navbar-gitlab').toggleClass('collapsed');
+$( document ).ready(function() {
+
+    $('.js-sidebar-collapse').on('click', function(event) {
+		collapse(event);
+	});
+
+	var collapse = function(event){
+		if(event != null){
+			event.preventDefault();
+		}
+		/* Find the i element and toggle the icon class */
+		$(this).find('i').toggleClass('fa-angle-left').toggleClass('fa-angle-right');
+		/* Find the following elements and toggle responsive classes to collapse the sidebar */
+		$('#sidebar-wrapper').toggleClass('collapsed');	
+		$('#sidebar-wrapper nav.nav-sidebar ul.nav > li.nav-item a.nav-link').find('span').toggleClass('hide');
+		$('.logo-text').toggleClass('hide');
+		$('.brand').toggleClass('collapsed');
+		$('#page-wrapper').toggleClass('collapsed');
+		$('header.navbar-gitlab').toggleClass('collapsed');
+	};
+
+	if(screen.width <= 576){
+		collapse(null);
+	}
 });
+
+
